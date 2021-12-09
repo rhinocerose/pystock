b60c1e3bcd92821a6317a0e9e75ef33301bf7358
rhinocerose <ashar.k.latif@gmail.com>
2021-12-09 12:08:08 -0500

    Started basic functions

--- src/pystock/pystock.py
+++ src/pystock/pystock.py
@@ -32,3 +32,49 @@ PREMARKET_OPEN = time(4,0)
 MARKET_OPEN = time(9,30)
 MARKET_CLOSE = time(16,0)
 POSTMARKET_CLOSE = time(20,0)
+
+def ticker_value(ticker: str) -> str:
+    """Get ticker price
+
+    Retrieves price of ticker
+
+    Args:
+        ticker: The ticker to get a stock price on and to display.
+
+    Returns:
+        output: Stock price and ticker of a stock with format 'TICKER': 'PRICE'.
+    """
+    ticker_price = si.get_live_price(ticker)
+    percentage = gain_loss(ticker, ticker_price)
+    output = ticker + ': ' + str(round(ticker_price, ROUND_NUMBER)) + percentage
+    if DEBUG:
+        print(str(output))
+    return output
+
+def gain_loss(ticker: str, today) -> str:
+    """Calculate percent change
+
+    Calculates percent price change from previous close and formats colorized string
+
+    Args:
+        ticker: The ticker to calculate percent change on.
+        today:  Current price of the cicker.
+
+    Returns:
+        output: Colorized directional percentage movement of the ticker
+    """
+    if DEBUG:
+        print("Starting gain_loss function")
+    start = last_trading_day()
+    yesterday = si.get_data(ticker, start_date = start).iloc[0]['close']
+    if DEBUG:
+        print("Last close: " + str(yesterday))
+    percentage = round(((100 * today) / yesterday) - 100, 2)
+    if DEBUG:
+        print("Percentage move:" + str(percentage))
+    if percentage >= 0:
+        color = POSITIVE_PERCENTAGE_COLOR
+    elif percentage < 0:
+        color = NEGATIVE_PERCENTAGE_COLOR
+    output = str('%{F' + color + '}' + str(percentage) + '%\%{F-}')
+    return output
